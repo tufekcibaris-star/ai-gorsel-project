ed03c251499cbbb99b88f3d0f437267d012f9135
tufekcibaris-star <tufekcibaris@gmail.com>
2025-11-18 11:07:43 +0300

    Remove commented CORS settings from handler
    Removed commented CORS settings for cleaner code.

--- api/generate-image.js
+++ api/generate-image.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // CORS ayarları
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');

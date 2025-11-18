be1dea6d8a7ab9a01f683986a2f80bf4f4eab3c8
tufekcibaris-star <tufekcibaris@gmail.com>
2025-11-18 11:27:39 +0300

    Update API endpoint for image generation

--- api/generate-image.js
+++ api/generate-image.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      'https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0',
+      'https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0',
       {
         method: 'POST',
         headers: {
